d76cb0eb3185a2ec7291989d8ef7db13709ce3fe
yhiaalawaje-maker <yhiaalawaje@gmail.com>
2026-05-12 09:06:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
-
 from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 
 BOT_TOKEN = "8258871315:AAHs4RjV2d5UR-IHZTB0_5eEIBamxTafXZE"
-CHAT_ID = "878214236"
 
 @app.route("/")
 def home():
@@ -15,15 +13,19 @@ def home():
 def webhook():
     data = request.json
 
-    message = data.get("message", "No message received")
+    chat_id = data["message"]["chat"]["id"]
+    text = data["message"]["text"]
 
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": CHAT_ID,
-        "text": message
+        "chat_id": chat_id,
+        "text": f"Received: {text}"
     }
 
     requests.post(telegram_url, json=payload)
 
     return {"status": "success"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
